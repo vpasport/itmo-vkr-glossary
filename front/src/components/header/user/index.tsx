7ad764c676c46d3e8297ea6ab78c21bdaf7256d2
Vyacheslav Pasportnikov <pasportnikovvyacheslav@gmail.com>
2023-04-12 21:59:15 +0300

    remove 123

--- front/src/components/header/user/index.tsx
+++ front/src/components/header/user/index.tsx
@@ -5,7 +5,7 @@ import styles from './user.module.scss'
 export const User: FC = () => {
   return (
     <a className={styles.user} href="https://vk.com/vpasport" target="_blank">
-      <span>Паспортников В.С. 123 (P42072)</span>
+      <span>Паспортников В.С. (P42072)</span>
       <img src="https://sun9-70.userapi.com/impg/_d3jGXuFnEspdPnqQfr-HYiyLeW792auSXffTg/KJaeZbAlMfM.jpg?size=2560x1707&quality=95&sign=8c7b1be6e9a2ed435a716b64fdb68925&type=album" />
     </a>
   )
